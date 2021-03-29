1960efc7438393d2b38ac95ee25affac4e647256
Nikita-Vikulov <n.vikulov92@yandex.ru>
2021-03-29 18:27:31 +0300

    HomeWork4

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,17 +3,21 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
+
+import com.google.android.material.radiobutton.MaterialRadioButton;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     String numbers = "";
     String view = "";
-    float result = 0;
+    double result = 0;
     int sign = 0;
 
     TextView textView;
@@ -39,7 +43,9 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(getAppTheme(R.style.MyCoolStyle));
         setContentView(R.layout.activity_main_calculator);
+        initThemeChooser();
 
         textView = (TextView) findViewById(R.id.textView);
         button_1 = (Button) findViewById(R.id.button_1);
@@ -79,6 +85,75 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         button_percent.setOnClickListener(this);
         button_equally.setOnClickListener(this);
         button_reset.setOnClickListener(this);
+    }
+    // Имя настроек
+    private static final String NameSharedPreference = "LOGIN";
+
+    // Имя параметра в настройках
+    private static final String appTheme = "APP_THEME";
+
+    private static final int MyCoolCodeStyle = 0;
+    private static final int AppThemeLightCodeStyle = 1;
+    private static final int AppThemeCodeStyle = 2;
+    private static final int AppThemeDarkCodeStyle = 3;
+
+    // Инициализация радиокнопок
+    private void initThemeChooser() {
+        initRadioButton(findViewById(R.id.radioButtonMyCoolStyle),
+                MyCoolCodeStyle);
+        initRadioButton(findViewById(R.id.radioButtonMaterialDark),
+                AppThemeDarkCodeStyle);
+        initRadioButton(findViewById(R.id.radioButtonMaterialLight),
+                AppThemeLightCodeStyle);
+        initRadioButton(findViewById(R.id.radioButtonMaterialLightDarkAction),
+                AppThemeCodeStyle);
+        RadioGroup rg = findViewById(R.id.radioButtons);
+        ((MaterialRadioButton)rg.getChildAt(getCodeStyle(MyCoolCodeStyle))).setChecked(true);
+    }
+
+    // Все инициализации кнопок очень похожи, поэтому создадим метод для переиспользования
+    private void initRadioButton(View button, final int codeStyle){
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setAppTheme(codeStyle);
+                recreate();
+            }
+        });
+    }
+
+    private int getAppTheme(int codeStyle) {
+        return codeStyleToStyleId(getCodeStyle(codeStyle));
+    }
+
+    // Чтение настроек, параметр «тема»
+    private int getCodeStyle(int codeStyle){
+        // Работаем через специальный класс сохранения и чтения настроек
+        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference, MODE_PRIVATE);
+        //Прочитать тему, если настройка не найдена - взять по умолчанию
+        return sharedPref.getInt(appTheme, codeStyle);
+    }
+
+    // Сохранение настроек
+    private void setAppTheme(int codeStyle) {
+        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference, MODE_PRIVATE);
+        // Настройки сохраняются посредством специального класса editor.
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(appTheme, codeStyle);
+        editor.apply();
+    }
+
+    private int codeStyleToStyleId(int codeStyle){
+        switch(codeStyle){
+            case AppThemeCodeStyle:
+                return R.style.AppTheme;
+            case AppThemeLightCodeStyle:
+                return R.style.AppThemeLight;
+            case AppThemeDarkCodeStyle:
+                return R.style.AppThemeDark;
+            default:
+                return R.style.MyCoolStyle;
+        }
     }
 
     @SuppressLint("NonConstantResourceId")
@@ -161,6 +236,13 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                 sign = 3;
                 textView.setText(view);
                 break;
+            case R.id.button_divide:
+                checkSign();
+                numbers = "";
+                view += "/";
+                sign = 4;
+                textView.setText(view);
+                break;
             case R.id.button_percent:
                 checkSign();
                 numbers = "";
@@ -185,10 +267,10 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     }
 
     private void checkSign() {
-        if (sign == 0) result = Float.parseFloat(numbers);
-        else if (sign == 1) result += Float.parseFloat(numbers);
-        else if (sign == 2) result -= Float.parseFloat(numbers);
-        else if (sign == 3) result *= Float.parseFloat(numbers);
-        else if (sign == 4) result /= Float.parseFloat(numbers);
+        if (sign == 0) result = Double.parseDouble(numbers);
+        else if (sign == 1) result += Double.parseDouble(numbers);
+        else if (sign == 2) result -= Double.parseDouble(numbers);
+        else if (sign == 3) result *= Double.parseDouble(numbers);
+        else if (sign == 4) result /= Double.parseDouble(numbers);
     }
 }
